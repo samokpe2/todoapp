c67d52005997b0ddcfc477bbd2175e4dc740f3e8
Samuel Okpe <38431147+samokpe2@users.noreply.github.com>
2022-08-12 23:10:40 +0100

    Created insert and added ajax

--- app.py
+++ app.py
@@ -1,18 +1,54 @@
-from flask import Flask, render_template
+import sys
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
+from flask_sqlalchemy import SQLAlchemy
+from flask import request
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/todoapp'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+  __tablename__ = 'todos'
+  id = db.Column(db.Integer, primary_key=True)
+  description = db.Column(db.String(), nullable=False)
+
+  def __repr__(self):
+    return f'<Todo {self.id} {self.description}>'
+
+db.create_all()
+
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+  error = False
+  body = {}
+  try:
+    description = request.get_json()['description']
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    body['description'] = todo.description
+  except:
+    error = True
+    db.session.rollback()
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+  if error:
+    abort (400)
+  else:
+    return jsonify(body)
+
+
+
 @app.route('/')
 def index():
-  return render_template('index.html', data=[{
-    'description': 'Todo 1'
-  }, {
-    'description': 'Todo 2'
-  }, {
-    'description': 'Todo 3'
-  }])
+  return render_template('index.html', data=Todo.query.all())
 
 
-#always include this at the bottom of your code (port 3000 is only necessary in workspaces)
 
+
+
+#always include this at the bottom of your code
 if __name__ == '__main__':
    app.run(host="0.0.0.0", port=3000)
